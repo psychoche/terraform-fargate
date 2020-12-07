0aac85cd5ac7e5fff5c3589e0f61fc600a6ab2d3
Pavel Letskii <pavel.letskii@vertoanalytics.com>
2020-12-07 11:14:10 +0200

    Changes in network. Add security groups

--- network.tf
+++ network.tf
@@ -41,14 +41,6 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
 
-# Route the public subnet traffic through the IGW
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_vpc.main.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.gw.id
-
-}
-
 # Create a NAT gateway with an Elastic IP for each private subnet to get internet connectivity
 resource "aws_eip" "gw" {
   vpc        = true
@@ -58,6 +50,20 @@ resource "aws_eip" "gw" {
 resource "aws_nat_gateway" "gw" {
   subnet_id     = aws_subnet.public[0].id
   allocation_id = aws_eip.gw.id
+}
+
+# Route the public subnet traffic through the IGW
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    "Name" = "public-route-table"
+  }
 }
 
 # Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
@@ -72,6 +78,13 @@ resource "aws_route_table" "private" {
   tags = {
     "Name" = "private-route-table"
   }
+}
+
+# Explicitly associate the newly created route tables to the public subnets (so they don't default to the main route table)
+resource "aws_route_table_association" "public" {
+  count          = var.az_count
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
 }
 
 # Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
